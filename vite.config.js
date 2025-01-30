5ad8394aae725e12719950dac7d63e632fa9a285
Roni <ronniiii@yahoo.com>
2025-01-30 10:41:47 +0000

    [CONFIG]: Update proxy links to use hosted db.json on npoint.io

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,9 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/categories": "https://a513-102-88-54-150.ngrok-free.app/", // Proxy requests for categories to your backend
-      "/transactions": "https://a513-102-88-54-150.ngrok-free.app/", // Proxy requests for transactions to your backend
-      "/name": "https://a513-102-88-54-150.ngrok-free.app/", // Proxy requests for transactions to your backend
+      "/categories": "https://api.npoint.io/f06685e7b9e1b0776d0b", // Proxy requests for categories to your backend
+      "/transactions": "https://api.npoint.io/f06685e7b9e1b0776d0b", // Proxy requests for transactions to your backend
+      "/name": "https://api.npoint.io/f06685e7b9e1b0776d0b", // Proxy requests for transactions to your backend
     },
   },
 });
