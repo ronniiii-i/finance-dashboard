0a76d33cfbdf98f439bdc0d0fee160e2cea245ee
Roni <ronniiiip@gmail.com>
2025-02-19 11:32:11 +0000

    fix routing issues

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: {
-      "/categories": "https://api.npoint.io/f06685e7b9e1b0776d0b", // Proxy requests for categories to your backend
-      "/transactions": "https://api.npoint.io/f06685e7b9e1b0776d0b", // Proxy requests for transactions to your backend
-      "/name": "https://api.npoint.io/f06685e7b9e1b0776d0b", // Proxy requests for transactions to your backend
-    },
+    // proxy: {
+    //   "/categories": "https://api.npoint.io/f06685e7b9e1b0776d0b", // Proxy requests for categories to your backend
+    //   "/transactions": "https://api.npoint.io/f06685e7b9e1b0776d0b", // Proxy requests for transactions to your backend
+    //   "/name": "https://api.npoint.io/f06685e7b9e1b0776d0b", // Proxy requests for transactions to your backend
+    // },
   },
 });
